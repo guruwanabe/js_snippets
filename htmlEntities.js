47a1e57fa918de869d69cf940d971a968f10f001
guruwanabe <bp.stanciu@gmail.com>
2017-05-16 00:50:57 +0300

    Update htmlEntities.js

--- htmlEntities.js
+++ htmlEntities.js
@@ -7,23 +7,27 @@
  * @return string
  */
 function htmlEntities($string, $quotes, $preserveAmp){
+	var string;
+
 	switch($quotes){
 		case "ENT_QUOTES":
-				//Will convert both double and single quotes.
-			return String($string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&quot;');
+			//Will convert both double and single quotes.
+			string = String($string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&quot;');
 			break;
 		case 'ENT_NOQUOTES':
-				//Will leave both double and single quotes unconverted
-			return String($string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+			//Will leave both double and single quotes unconverted
+			string = String($string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
 			break;
 		case 'ENT_COMPAT':
-				//Will convert double-quotes and leave single-quotes alone.
-			return String($string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+			//Will convert double-quotes and leave single-quotes alone.
+			string = String($string).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 			break;
 		default:
 	}
 
 	if($preserveAmp){
-		return String($string).replace(/&amp;/g, '&')
+		string = String($string).replace(/&amp;/g, '&')
 	}
+
+	return string;
 }
